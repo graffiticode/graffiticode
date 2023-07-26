554e2c4fbb5035f226dca62dd97c135bafe8671b
Jeff Dyer <jeffdyer@acm.org>
2023-07-26 13:51:36 -0700

    Fix compile store for case with data that is arrays of arrays

--- packages/api/src/storage/compile.js
+++ packages/api/src/storage/compile.js
@@ -1,11 +1,28 @@
 import { admin } from "./firebase.js";
 
+function encodeCompileData(compile) {
+  // We do this because not all objects are be stored natively in firestore.
+  // E.g. arrays of arrays are not compatible.
+  return {
+    ...compile,
+    data: JSON.stringify(compile.data),
+  };
+}
+
+function decodeCompileData(compile) {
+  return {
+    ...compile,
+    data: JSON.parse(compile.data),
+  };
+}
+
 const buildCompileCreate = ({ db }) => async ({ id, compile, auth }) => {
   const compileRef = db.doc(`compiles/${id}`);
   const compileDoc = await compileRef.get();
-
+  console.log("compileCreate() id=" + id);
+  console.log("compileCreate() compile=" + JSON.stringify(compile, null, 2));
   if (!compileDoc.exists) {
-    await compileRef.set(compile);
+    await compileRef.set(encodeCompileData(compile));
   }
   return id;
 };
@@ -17,7 +34,7 @@ const buildCompileGet = ({ db }) => async ({ id, auth }) => {
   if (!compileDoc.exists) {
     return undefined;
   }
-  return compileDoc.data();
+  return decodeCompileData(compileDoc.data());
 };
 
 export const buildCompileStorer = () => {
