a7061d02a93b394779146133f237e89d7a91cdfb
Kevin Dyer <kevin.m.dyer@gmail.com>
2023-09-06 21:21:25 -0600

    feat: add tests for verifyToken

--- packages/auth/src/services/auth.spec.js
+++ packages/auth/src/services/auth.spec.js
@@ -1,7 +1,10 @@
+import { UnauthenticatedError } from "@graffiticode/common/errors";
 import { createLocalJWKSet } from "jose";
 import { createApp } from "../app.js";
-import { cleanUpFirebase } from "../testing/firebase.js";
+import { cleanUpFirebase, signInAndGetIdToken } from "../testing/firebase.js";
 import { buildVerifyAccessToken } from "./auth.js";
+
+const uid = "abc123";
 
 describe("services/auth", () => {
   let appDeps;
@@ -14,7 +17,6 @@ describe("services/auth", () => {
   });
 
   it("should propagate additional claims through the refresh token", async () => {
-    const uid = "abc123";
     const additionalClaims = { foo: "bar" };
     const { refreshToken } = await appDeps.authService.generateTokens({ uid, additionalClaims });
 
@@ -24,5 +26,31 @@ describe("services/auth", () => {
     const JWKS = createLocalJWKSet({ keys: certs });
     const { payload } = await buildVerifyAccessToken({ JWKS })(accessToken);
     expect(payload).toHaveProperty("foo", "bar");
+  });
+
+  describe("verifyToken", () => {
+    it("should return auth context for accessToken", async () => {
+      const token = await appDeps.authService.createAccessToken({ uid });
+
+      const authContext = await appDeps.authService.verifyToken({ token });
+
+      expect(authContext).toHaveProperty("uid", uid);
+    });
+
+    it("should return auth context for firebaseCustomToken", async () => {
+      const customToken = await appDeps.authService.createFirebaseCustomToken({ uid });
+      const token = await signInAndGetIdToken(customToken);
+
+      const authContext = await appDeps.authService.verifyToken({ token });
+
+      expect(authContext).toHaveProperty("uid", uid);
+    });
+
+    it("should throw Unauthenticated for invalid access token", async () => {
+      let token = await appDeps.authService.createAccessToken({ uid });
+      token = [...token.split(".").slice(0, 2), "not-a-valid-signature"].join(".");
+
+      await expect(appDeps.authService.verifyToken({ token })).rejects.toThrow(UnauthenticatedError);
+    });
   });
 });
