79060bf2bda16b7365770be31bf85d353252bd66
Jeff Dyer <jeffdyer@acm.org>
2025-05-09 12:38:37 -0700

    Fix tests

--- packages/api/src/lang/compile.js
+++ packages/api/src/lang/compile.js
@@ -1,18 +1,5 @@
 export const buildCompile = ({ getBaseUrlForLanguage, bent }) => async (lang, req) => {
   const baseUrl = getBaseUrlForLanguage(lang);
-  const compilePost = bent(baseUrl, "POST", "json", 200, 202, 500);
-  try {
-    return await compilePost("/compile", req);
-  } catch (err) {
-    console.error("Status:", err.statusCode); // 500
-    console.error("Headers:", err.headers); // same object you pasted
-    // body helpers ↓ are *async*
-    try {
-      const message = await err.text(); // or err.json()
-      console.error("Server said:", message);
-    } catch (e) {
-      console.error("Could not read body:", e);
-    }
-    return null;
-  }
+  const compilePost = bent(baseUrl, "POST", "json", 200, 202);
+  return await compilePost("/compile", req);
 };
