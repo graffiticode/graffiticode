554c809b65b8e19ffa0d843ee773c57262abaf3e
Jeff Dyer <jeffdyer@acm.org>
2025-05-09 12:23:50 -0700

    Lint fixes

--- packages/api/src/lang/compile.js
+++ packages/api/src/lang/compile.js
@@ -4,14 +4,14 @@ export const buildCompile = ({ getBaseUrlForLanguage, bent }) => async (lang, re
   try {
     return await compilePost("/compile", req);
   } catch (err) {
-    console.error('Status:', err.statusCode);       // 500
-    console.error('Headers:', err.headers);         // same object you pasted
+    console.error("Status:", err.statusCode); // 500
+    console.error("Headers:", err.headers); // same object you pasted
     // body helpers ↓ are *async*
     try {
-      const message = await err.text();             // or err.json()
-      console.error('Server said:', message);
+      const message = await err.text(); // or err.json()
+      console.error("Server said:", message);
     } catch (e) {
-      console.error('Could not read body:', e);
+      console.error("Could not read body:", e);
     }
     return null;
   }
