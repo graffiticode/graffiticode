12d8d92ea5e1ebd708b4718f307a7a2edc62ff79
Jeff Dyer <jeffdyer@acm.org>
2025-05-09 16:36:47 -0700

    Don't Accept 500s for now. Breaks tests

--- packages/api/src/lang/compile.js
+++ packages/api/src/lang/compile.js
@@ -1,5 +1,5 @@
 export const buildCompile = ({ getBaseUrlForLanguage, bent }) => async (lang, req) => {
   const baseUrl = getBaseUrlForLanguage(lang);
-  const compilePost = bent(baseUrl, "POST", "json", 200, 202, 500);
+  const compilePost = bent(baseUrl, "POST", "json", 200, 202);
   return await compilePost("/compile", req);
 };
