49572e9afab5b0b7ab9ce563a3f962d55b30fb4f
Jeff Dyer <jeffdyer@acm.org>
2026-03-17 11:46:38 -0700

    Add test

--- packages/parser/src/folder.spec.js
+++ packages/parser/src/folder.spec.js
@@ -46,4 +46,33 @@ describe("folder", () => {
     expect(n2.tag).toBe("NUM");
     expect(n2.elts[0]).toBe("3");
   });
+
+  it("should pass 'pow 2 3' through as POW node", () => {
+    const ctx = {
+      state: {
+        nodePool: ["unused"],
+        nodeStack: [],
+        nodeStackStack: [],
+        nodeMap: {},
+        env: [{ name: "global", lexicon: {} }]
+      }
+    };
+
+    const n1Id = Ast.intern(ctx, { tag: "NUM", elts: ["2"] });
+    const n2Id = Ast.intern(ctx, { tag: "NUM", elts: ["3"] });
+    const powNodeId = Ast.intern(ctx, { tag: "POW", elts: [n1Id, n2Id] });
+
+    Folder.fold(ctx, powNodeId);
+    const resultId = ctx.state.nodeStack.pop();
+    const resultNode = ctx.state.nodePool[resultId];
+
+    expect(resultNode.tag).toBe("POW");
+    expect(resultNode.elts.length).toBe(2);
+    const n1 = Ast.node(ctx, resultNode.elts[0]);
+    const n2 = Ast.node(ctx, resultNode.elts[1]);
+    expect(n1.tag).toBe("NUM");
+    expect(n1.elts[0]).toBe("2");
+    expect(n2.tag).toBe("NUM");
+    expect(n2.elts[0]).toBe("3");
+  });
 });
